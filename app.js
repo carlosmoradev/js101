9e13f3055326b6bfa6261f98c369f6775cf8ab3a
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2018-08-21 17:49:49 -0500

    requerimientos algoritmo

---
+++ app.js
@@ -0,0 +1,12 @@
+/*
+
+Crear un algoritmo que permita determinar
+la calificacion en letras de un alumno.
+
+Si la nota se encuentra entre 100 a 90 = A
+Si la nota se encuentra entre 80 a 89 = A
+Si la nota se encuentra entre 70 a 79 = A
+Si la nota se encuentra entre 60 a 69 = A
+Si la nota es inferior a 60 = F
+
+*/
