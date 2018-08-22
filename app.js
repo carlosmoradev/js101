cf0f393aa349cbaea6e9f0bba0610cf1d819e5ef
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2018-08-21 19:17:57 -0500

    optimizando mensaje final

--- app.js
+++ app.js
@@ -4,9 +4,36 @@ Crear un algoritmo que permita determinar
 la calificacion en letras de un alumno.
 
 Si la nota se encuentra entre 100 a 90 = A
-Si la nota se encuentra entre 80 a 89 = A
-Si la nota se encuentra entre 70 a 79 = A
-Si la nota se encuentra entre 60 a 69 = A
+Si la nota se encuentra entre 80 a 89 = B
+Si la nota se encuentra entre 70 a 79 = C
+Si la nota se encuentra entre 60 a 69 = D
 Si la nota es inferior a 60 = F
 
 */
+
+let nota = 90;
+
+let calificacion = "N";
+
+if (nota >= 90) {
+
+    calificacion = "A";
+
+} else if (nota >= 80) {
+
+    calificacion = "B";
+
+} else if (nota >= 70) {
+
+    calificacion = "C";
+
+} else if (nota >= 60) {
+
+    calificacion = "D";
+
+} else {
+
+    calificacion = "F";
+}
+
+console.log(`La calificacion obtenida es ${calificacion}`);
